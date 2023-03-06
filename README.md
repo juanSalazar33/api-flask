15f827d13d745e134095f378f508040741f6a386
Juan José <juancrogollo@gmail.com>
2023-03-05 23:19:14 -0400

    add CORS, update README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# api-flask
+# simple api-flask 
+
+# to turn on the server follow these steps!:
+    1. install and run Docker on your computer: https://www.docker.com/
+    3. in cmd terminal run: "docker build -t flaskapp ."
+    4. in cmd terminal run: "docker run -p 7000:4001 -d flaskapp"
+
+# if you want developer i recomend use virtual environment:
+    python3 -m venv env
+
+# if you add new dependencies run:
+    pip freeze > requirments.txt
+
--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 #IMPORTS & DECLARATIONS
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from task import tasks
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-type'
 route_base = '/tasks'
+
 #ROUTERS
 @app.route('/')
 def test():
     return jsonify({"message": "server on work on!"})
 
 @app.route(route_base, methods=['GET'])
-def getTasks():
-    return jsonify({'LISTA DE TAREAS':tasks})
+@cross_origin()
+def getActivities():
+    return jsonify({'activities':tasks})
 
 @app.route(route_base+'/<int:task_id>', methods=['GET'])
-def getTask(task_id): 
+def getActivity(task_id): 
     taskFound = [task2 for task2 in tasks if task2['activityId'] == task_id]
 
     if(len (taskFound) > 0):
-        return jsonify({'TAREA ENCONTRADA':taskFound[0]})
-    return jsonify({"message": "TAREA NO ENCONTRADA"})
+        return jsonify({'ACTIVIDAD ENCONTRADA':taskFound[0]})
+    return jsonify({"message": "ACTIVIDAD NO ENCONTRADA"})
 
 @app.route(route_base, methods=['POST'])
-def addTask():
+def addActivity():
     json = request.get_json(force=True)
     new_task  =  {
         "activityId" : json['activityId'],
@@ -33,7 +38,7 @@ def addTask():
     }
     tasks.append(new_task)
     print(new_task)
-    return 'recived'
+    return jsonify({"message": "ACTIVIDAD CREADA"})
 #START SERVER
 if __name__  == '__main__':
     app.run(host="0.0.0.0",  debug=True, port=4001)
